d197708afac05cfdcc972992309c86785ac7c123
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2021-09-03 00:21:33 +0300

    CountDown Done!

---
+++ CountDown/WindowsFormsApp1/CowntDownForm.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class CountDownTimer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblCountDown = new System.Windows.Forms.Label();
+            this.lblNrSeconds = new System.Windows.Forms.Label();
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnPlus5 = new System.Windows.Forms.Button();
+            this.btnMinus5 = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblCountDown
+            // 
+            this.lblCountDown.Font = new System.Drawing.Font("Tahoma", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCountDown.ForeColor = System.Drawing.Color.White;
+            this.lblCountDown.Location = new System.Drawing.Point(3, 3);
+            this.lblCountDown.Name = "lblCountDown";
+            this.lblCountDown.Size = new System.Drawing.Size(739, 75);
+            this.lblCountDown.TabIndex = 0;
+            this.lblCountDown.Text = "Count Down Timer";
+            this.lblCountDown.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblNrSeconds
+            // 
+            this.lblNrSeconds.Font = new System.Drawing.Font("Tahoma", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNrSeconds.ForeColor = System.Drawing.Color.White;
+            this.lblNrSeconds.Location = new System.Drawing.Point(3, 385);
+            this.lblNrSeconds.Name = "lblNrSeconds";
+            this.lblNrSeconds.Size = new System.Drawing.Size(739, 75);
+            this.lblNrSeconds.TabIndex = 1;
+            this.lblNrSeconds.Text = "x Seconds";
+            this.lblNrSeconds.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textInput
+            // 
+            this.textInput.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textInput.Location = new System.Drawing.Point(253, 107);
+            this.textInput.Name = "textInput";
+            this.textInput.Size = new System.Drawing.Size(259, 32);
+            this.textInput.TabIndex = 2;
+            this.textInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textInput.TextChanged += new System.EventHandler(this.textInput_TextChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(253, 181);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(115, 54);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(397, 181);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(115, 54);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(253, 306);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(259, 54);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnPlus5
+            // 
+            this.btnPlus5.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlus5.Location = new System.Drawing.Point(253, 246);
+            this.btnPlus5.Name = "btnPlus5";
+            this.btnPlus5.Size = new System.Drawing.Size(115, 54);
+            this.btnPlus5.TabIndex = 6;
+            this.btnPlus5.Text = "+5";
+            this.btnPlus5.UseVisualStyleBackColor = true;
+            this.btnPlus5.Click += new System.EventHandler(this.btnPlus5_Click);
+            // 
+            // btnMinus5
+            // 
+            this.btnMinus5.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinus5.Location = new System.Drawing.Point(397, 246);
+            this.btnMinus5.Name = "btnMinus5";
+            this.btnMinus5.Size = new System.Drawing.Size(115, 54);
+            this.btnMinus5.TabIndex = 7;
+            this.btnMinus5.Text = "-5";
+            this.btnMinus5.UseVisualStyleBackColor = true;
+            this.btnMinus5.Click += new System.EventHandler(this.btnMinus5_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // CountDownTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(749, 480);
+            this.Controls.Add(this.btnMinus5);
+            this.Controls.Add(this.btnPlus5);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.textInput);
+            this.Controls.Add(this.lblNrSeconds);
+            this.Controls.Add(this.lblCountDown);
+            this.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CountDownTimer";
+            this.Padding = new System.Windows.Forms.Padding(20);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Count Down";
+            this.Load += new System.EventHandler(this.CountDownTimer_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCountDown;
+        private System.Windows.Forms.Label lblNrSeconds;
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnPlus5;
+        private System.Windows.Forms.Button btnMinus5;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ CountDown/WindowsFormsApp1/CowntDownForm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class CountDownTimer : Form
+    {
+
+        private int seconds = 0;
+        int number;
+        public CountDownTimer()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeLabel()
+        {
+            lblNrSeconds.Text = seconds + " Seconds";
+        }
+
+        private void CountDownTimer_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textInput_TextChanged(object sender, EventArgs e)
+        {
+            bool success = int.TryParse(textInput.Text, out number);
+
+            if (success)
+            {
+                seconds = number;
+            } else
+            {
+                seconds = 60;
+            }
+
+            ChangeLabel();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if(seconds > 0)
+            {
+                seconds -= 1;
+                ChangeLabel();
+            } else
+            {
+                timer.Stop();
+                lblNrSeconds.Text = "Time is up!";
+
+            }
+        }
+
+        private void btnPlus5_Click(object sender, EventArgs e)
+        {
+            seconds += 5;
+            ChangeLabel();
+        }
+
+        private void btnMinus5_Click(object sender, EventArgs e)
+        {
+            seconds -= 5;
+            ChangeLabel();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            seconds = 60;
+            ChangeLabel();
+        }
+
+       
+    }
+}
